c5189d60d56644c33e46b7b451fb892b3408e4c6
Henry Whitaker <henrywhitaker3@outlook.com>
2025-08-22 15:42:22 +0100

    test: check errors are sent through
    Signed-off-by: Henry Whitaker <henrywhitaker3@outlook.com>

--- comms/comms_test.go
+++ comms/comms_test.go
@@ -2,6 +2,7 @@ package comms_test
 
 import (
 	"context"
+	"fmt"
 	"strings"
 	"testing"
 	"time"
@@ -12,11 +13,45 @@ import (
 )
 
 func TestItRespondsToMessages(t *testing.T) {
-	nats, cancel := test.Nats(t)
+	send, _, cancel := setup(t)
 	defer cancel()
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
 	defer cancel()
+
+	_, err := send.Send(ctx, comms.Topic("example"), []byte("bongo"))
+	require.NotNil(t, err)
+
+	reply, err := send.Send(ctx, comms.Topic("test"), []byte("bongo"))
+	require.Nil(t, err)
+	require.Equal(t, "BONGO", string(reply))
+}
+
+func TestItForwardsErrors(t *testing.T) {
+	send, respond, cancel := setup(t)
+	defer cancel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	go func() {
+		if err := respond.Listen(ctx, comms.Topic("example"), func(ctx context.Context, b []byte) ([]byte, error) {
+			return nil, fmt.Errorf("something went wrong")
+		}); err != nil {
+			panic(err)
+		}
+	}()
+	time.Sleep(time.Second)
+
+	_, err := send.Send(ctx, comms.Topic("example"), nil)
+	require.NotNil(t, err)
+	require.Equal(t, "something went wrong", err.Error())
+}
+
+func setup(t *testing.T) (*comms.Sender, *comms.Responder, context.CancelFunc) {
+	nats, cancelNats := test.Nats(t)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
 
 	send, err := comms.NewSender(comms.SenderOpts{
 		URL: nats,
@@ -28,13 +63,10 @@ func TestItRespondsToMessages(t *testing.T) {
 	})
 	require.Nil(t, err)
 
-	_, err = send.Send(ctx, comms.Topic("example"), []byte("bongo"))
-	require.NotNil(t, err)
-
 	go func() {
 		if err := respond.Listen(
 			ctx,
-			comms.Topic("example"),
+			comms.Topic("test"),
 			func(ctx context.Context, b []byte) ([]byte, error) {
 				return []byte(strings.ToUpper(string(b))), nil
 			},
@@ -45,7 +77,8 @@ func TestItRespondsToMessages(t *testing.T) {
 
 	time.Sleep(time.Second)
 
-	reply, err := send.Send(ctx, comms.Topic("example"), []byte("bongo"))
-	require.Nil(t, err)
-	require.Equal(t, "BONGO", string(reply))
+	return send, respond, func() {
+		cancel()
+		cancelNats()
+	}
 }
