3f6f9ce2e85b277dff371e0e396578057566c482
Henry Whitaker <henrywhitaker3@outlook.com>
2025-08-30 11:12:40 +0100

    feat: add crypto funcs
    Signed-off-by: Henry Whitaker <henrywhitaker3@outlook.com>

---
+++ test/faker.go
@@ -0,0 +1,27 @@
+package test
+
+import "github.com/brianvoe/gofakeit/v7"
+
+func init() {
+	gofakeit.Seed(0)
+}
+
+func Word() string {
+	return gofakeit.Word()
+}
+
+func Sentence(words int) string {
+	return gofakeit.Sentence(words)
+}
+
+func Email() string {
+	return gofakeit.Email()
+}
+
+func Letters(length int) string {
+	return gofakeit.LetterN(uint(length))
+}
+
+func Name() string {
+	return gofakeit.Name()
+}
